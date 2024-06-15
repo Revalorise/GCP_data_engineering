a6f95383724f5168d090c526b6bffbf418acc9e5
Bon <Blackball007b@gmail.com>
2024-06-15 07:30:42 +0700

    major updates on setup.py script

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 import os
 from google.cloud import storage, bigquery
+from google.api_core.exceptions import Conflict
 from dotenv import load_dotenv
 
-credential_path = "./key.json"
+credential_path = "./gcp-data-engineering-key.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 load_dotenv()
 project_id = os.getenv('PROJECT_ID')
-dataset_id = "google_trends"
-table_id = [
-    "international_top_rising_terms",
-    "international_top_terms",
-    "top_rising_terms",
-    "top_terms"
-]
+
+country_name = ["Thailand", "Japan", "United States"]
+country_code = ["TH", "JA", "US"]
+
 source_bucket = f"{project_id}-source"
 
 
@@ -27,48 +25,56 @@ def create_source_bucket(project_id: str):
     """
 
     storage_client = storage.Client()
-    buckets = storage_client.list_buckets()
     bucket_name = f"{project_id}-source"
 
-    if bucket_name in buckets:
-        print(f"Bucket {bucket_name} already exists")
-    else:
+    try:
         bucket = storage_client.create_bucket(bucket_name)
         print(f"Bucket {bucket.name} created")
+    except Conflict:
+        print(f"Bucket {bucket_name} already exists")
 
     assert storage_client.get_bucket(bucket_name).exists()
 
 
-def export_bigquery_table_to_source_bucket(project_id: str,
-                                           dataset_id: str,
-                                           table_id: list,
-                                           source_bucket: str):
+def export_bigquery_result_to_source_bucket(country_name: list,
+                                            country_code: list):
     """
-    Export bigquery table to source bucket
+    Export bigquery queries to source bucket in csv format.
+
+    This function only supports in exporting Census bureau international mortality
+    and life expectancy daya
 
     Parameters:
-    project_id (str): Google cloud project id.
-    dataset_id (str): Initialized client object from Google cloud bigquery module.
-    table_id   (str):
-    source_bucket (str):
+    country_name (list): List of country name to be exported.
+    country_code (list): List of country code e.g. ("TH", "JP").
     """
 
     bigquery_client = bigquery.Client()
+    storage_client = storage.Client()
 
-    for table in table_id:
-        destination_uri = f"gs://{source_bucket}/{table}.csv"
-        dataset_ref = bigquery.DatasetReference(project_id, dataset_id)
-        table_ref = dataset_ref.table(table)
+    for i in range(len(country_name)):
+        query = f"""
+            SELECT * 
+            FROM `bigquery-public-data.census_bureau_international.mortality_life_expectancy` 
+            WHERE country_name = "{country_name[i]}"
+            ORDER BY year 
+            DESC LIMIT 100;
+        """
 
-        extract_job = bigquery_client.extract_table(
-            table_ref,
-            destination_uri,
-            location="US",
-        )
-        extract_job.result()
+        file_name = f"{country_code[i]}_mortality_life_expectancy.csv"
 
-        print(f"Exported {table} to {source_bucket}.")
+        query_job = bigquery_client.query(query)
+        destination_blob = storage_client.bucket(source_bucket).blob(file_name)
+        destination_blob.content_type = "text/csv"
+        query_job.result().to_dataframe().to_csv(destination_blob.open('w'), index=False)
+
+        bucket = storage_client.get_bucket(source_bucket)
+        blob = bucket.get_blob(file_name)
+        print(f'The query results are exported to {blob.public_url}')
+
+    print("Export completed")
 
 
 if __name__ == "__main__":
     create_source_bucket(project_id)
+    export_bigquery_result_to_source_bucket(country_name, country_code)
